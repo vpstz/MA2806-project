5757863a2f1ee90b4ce580613a79264b6ffb2651
vpstz <vpstz214@gmail.com>
2026-04-23 17:50:16 +0100

    added navigation bar and made main function in scripts.js

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function drawChart(data, element, ) {
+    // TODO: finish draw chart function
+}
+
+function readData(data) {
+
+}
+
+
+function main() {
+    let elements = document.getElementsByClassName('chart');
+
+}
+
+main();
