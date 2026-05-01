c5d0acebfb1b96bbea3582b1504ed4aa80de0c85
vpstz <vpstz214@gmail.com>
2026-05-01 19:40:17 +0100

    added death chart, decided to go with single chart at a time over multi-pie view

--- data_loader.js
+++ data_loader.js
@@ -1,5 +1,5 @@
 
-function load_csv(text) {
+function parse_csv(csv) {
     // const parsed = Papa.parse(text, {header: true});
     // results = {}
     //
@@ -18,11 +18,12 @@ function load_csv(text) {
     // })
     // console.log(results);
     // return results;
-    let results = Papa.parse(text, {
-        header: true,
-        // dynamicTyping: true,
-    });
-    const data = results.data;
+    // let results = Papa.parse(text, {
+    //     header: true,
+    //     // dynamicTyping: true,
+    // });
+    const data = csv.data;
+
 
     // Detect columns
     const columns = Object.keys(data[0]);
@@ -56,16 +57,193 @@ function load_csv(text) {
 
         return out;
     });
+
     return {dataset: datasets, min: min, max: max};
+}
+
+function gen_config(type, data, options) {
+    return {
+        type: type,
+        data: data,
+        options: options
+    }
+}
+
+function gen_dataset(label) {
+    return {
+        label: label,
+        data: []
+    }
+}
+
+function gen_options(title, subtitle) {
+    return {
+        aspectRatio: 0,
+            responsive: true,
+        plugins: {
+        legend: {
+            position: 'right',
+            align: 'start'
+        },
+        subtitle: {
+            position: 'bottom',
+            font: {
+                size: 16,
+            },
+            display: true,
+            align: 'start',
+            text: subtitle
+        }
+    }
+    }
+}
+
+function read_death(csv) {
+    const result = csv.data;
+
+
+    let totals = {
+        men: 0,
+        women: 0,
+        total: 0,
+    }
+
+    let datasets = {
+        total: [gen_dataset('Total')],
+        men: [gen_dataset('Men')],
+        women: [gen_dataset('Women')],
+
+    }
+    let labels = [];
+
+    for (let item of result) {
+        console.log(item);
+        labels.push(item['cause']);
+        // deaths.labels.push(item['cause']);
+        for (let i in datasets) {
+            datasets[i][0].data.push(item[i]);
+            totals[i] += Number(item[i]);
+        }
+    }
+    let configs = {};
+    let format = new Intl.NumberFormat('en-GB')
+    for (let item in datasets) {
+        configs[item] = gen_config('pie', {labels: labels, datasets:datasets[item]},
+            gen_options(undefined, "Total deaths: " + format.format(totals[item]))
+        );
+    }
+
+    // deaths.datasets = Object.values(datasets);
+
+    return configs;
+}
+
+function multi_chart(csv, type) {
+    const result = csv.data;
+    if (type === undefined) {
+        type = 'doughnut'
+    }
+    let data = {
+        labels: [],
+        datasets: [
+
+        ]
+    }
+    let datasets = {
+        total: {
+            label: 'Total',
+            data: []
+        },
+        women: {
+            label: 'Women',
+            data: []
+        },
+        men: {
+            label: 'Men',
+            data: []
+        },
+
+    }
+    for (let item of result) {
+        console.log(item);
+        // deaths.labels.push(item['cause']);
+        for (let i in datasets) {
+            datasets[i].data.push(item[i]);
+            data.labels.push(item['cause']);
+        }
+    }
+    data.datasets = Object.values(datasets);
+    const config = {
+        type: 'pie',
+        data: data,
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    labels: {
+                        generateLabels: function(chart) {
+                            // Get the default label list
+                            const original = Chart.overrides.pie.plugins.legend.labels.generateLabels;
+                            const labelsOriginal = original.call(this, chart);
+
+                            // Build an array of colors used in the datasets of the chart
+                            let datasetColors = chart.data.datasets.map(function(e) {
+                                return e.backgroundColor;
+                            });
+                            datasetColors = datasetColors.flat();
+
+                            // Modify the color and hide state of each label
+                            labelsOriginal.forEach(label => {
+                                // There are twice as many labels as there are datasets. This converts the label index into the corresponding dataset index
+                                label.datasetIndex = (label.index - label.index % 2) / 2;
+
+                                // The hidden state must match the dataset's hidden state
+                                label.hidden = !chart.isDatasetVisible(label.datasetIndex);
+
+                                // Change the color to match the dataset
+                                label.fillStyle = datasetColors[label.index];
+                            });
+
+                            return labelsOriginal;
+                        }
+                    },
+                    onClick: function(mouseEvent, legendItem, legend) {
+                        // toggle the visibility of the dataset from what it currently is
+                        legend.chart.getDatasetMeta(
+                            legendItem.datasetIndex
+                        ).hidden = legend.chart.isDatasetVisible(legendItem.datasetIndex);
+                        legend.chart.update();
+                    }
+                },
+                tooltip: {
+                    callbacks: {
+                        title: function(context) {
+                            const labelIndex = (context[0].datasetIndex * 2) + context[0].dataIndex;
+                            return context[0].chart.data.labels[labelIndex] + ': ' + context[0].formattedValue;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    return config;
+
+
 }
 
 
 
-
-async function readData(uri) {
+async function readData(uri, data_type) {
     let result = await fetch(uri);
-    result = await result.text();
-    return load_csv(result);
+    result = Papa.parse(await result.text(), {
+        header: true,
+    })
+    if (data_type === null || data_type === undefined|| data_type === 'usage') {
+        return parse_csv(result);
+    } else if (data_type === 'deaths') {
+        return read_death(result);
+    }
 }
 
 async function load_datasets() {
@@ -75,9 +253,10 @@ async function load_datasets() {
             regional: await readData("datasets/regional-usage.csv"),
             sex: await readData("datasets/usage-by-sex.csv"),
             age: await readData("datasets/by-age.csv"),
-
-        }
+        },
+        deaths: await readData("datasets/death-rate.csv", "deaths"),
     };
+    console.log(datasets.usage.age);
     return datasets;
     // regional dataset
 
--- scripts.js
+++ scripts.js
@@ -32,6 +32,31 @@ function drawChart(element, data) {
         });
 }
 
+function smart_update(chart, config) {
+    const keys = Object.keys(config);
+    if (keys.includes('options')) {
+        chart.options = config.options;
+    }
+    if (keys.includes('data')) {
+        chart.data = config.data;
+    }
+    if (keys.includes('type')) {
+        chart.type = config.type;
+    }
+    chart.update();
+}
+
+function button_action(chart, config, buttons, category) {
+    smart_update(chart, config[category]);
+
+    for (const btn in buttons) {
+        if (buttons[btn].disabled) {
+            buttons[btn].disabled = false;
+        }
+    }
+    buttons[category].disabled = true;
+}
+
 
 function updateChart(chart, data) {
     chart.data = {datasets: data.dataset};
@@ -52,11 +77,8 @@ function changeUsageGraph(button, chart, data) {
     button.disabled = true;
 }
 
-async function main() {
-    let chart_elements = document.getElementsByClassName('chart');
-    const datasets = await load_datasets();
-
-    const usage_chart = drawChart(chart_elements[0], datasets.usage.ethnicity)
+function load_usage(datasets) {
+    const usage_chart = drawChart(document.getElementById("cigarette-usage-chart"), datasets.ethnicity)
     // console.log(data)
     // drawChart(chart_elements[0], data);
 
@@ -65,12 +87,93 @@ async function main() {
     const sex_btn = document.getElementById('sex-button')
     const age_btn = document.getElementById('age-button');
 
-    ethnicity_btn.onclick = () => {changeUsageGraph(ethnicity_btn, usage_chart, datasets.usage.ethnicity);};
-    region_btn.onclick = () => {changeUsageGraph(region_btn, usage_chart, datasets.usage.regional);};
-    sex_btn.onclick = () => {changeUsageGraph(sex_btn, usage_chart, datasets.usage.sex)}
-    age_btn.onclick = () => {changeUsageGraph(age_btn, usage_chart, datasets.usage.age);};
+    console.log(datasets.sex);
+
+    ethnicity_btn.onclick = () => {changeUsageGraph(ethnicity_btn, usage_chart, datasets.ethnicity);};
+    region_btn.onclick = () => {changeUsageGraph(region_btn, usage_chart, datasets.regional);};
+    sex_btn.onclick = () => {changeUsageGraph(sex_btn, usage_chart, datasets.sex)}
+    age_btn.onclick = () => {changeUsageGraph(age_btn, usage_chart, datasets.age);};
     ethnicity_btn.click();
-    // ethnicity_btn.click();
+}
+
+function draw_pie(element, dataset, labels) {
+    const data = {
+        labels: labels,
+        datasets: [dataset]
+    }
+    new Chart(element, {
+        type: 'doughnut',
+        data: data,
+    })
+}
+
+
+
+function load_death_chart(datasets) {
+    const chart_group = document.getElementById('death-charts');
+    const chart_elements = {
+        total: document.getElementById('total-death'),
+        women: document.getElementById('women-death'),
+        men: document.getElementById('men-death'),
+    }
+
+    const death_buttons = {
+        total: document.getElementById('total-death-btn'),
+        women: document.getElementById('women-death-btn'),
+        men: document.getElementById('men-death-btn'),
+    }
+
+    let chart = new Chart(document.getElementById('smart-death-chart'), {
+        type: 'doughnut',
+        data: {},
+        options: {
+            aspectRatio: 1,
+            legend: {
+                position: 'right',
+                alight: 'middle'
+            }
+        }
+    });
+
+    for (let i in death_buttons) {
+        death_buttons[i].onclick = () => {button_action(chart, datasets, death_buttons, i)};
+    }
+    Object.values(death_buttons)[0].click();
+    // for (let data in datasets.datasets) {
+    //     // draw_pie(Object.values(chart_elements)[data], datasets.datasets[data], datasets.labels);
+    //
+    // }
+    // Multi layer
+    // let config = {
+    //     type: 'pie',
+    //     data: datasets,
+    //     options: {
+    //         responsive: true,
+    //         // plugins: {
+    //         //     legend: {
+    //         //         labels: {
+    //         //
+    //         //         }
+    //         //     }
+    //         // }
+    //     }
+    // }
+    // new Chart(chart_elements.men, config);
+
+}
+
+function load_multi_chart(datasets) {
+    const element = document.getElementById('multi-chart');
+
+    new Chart(element, datasets);
+}
+
+async function main() {
+    // let chart_elements = document.getElementsByClassName('chart');
+    const datasets = await load_datasets();
+
+    load_usage(datasets.usage);
+    load_death_chart(datasets.deaths);
 
 }
 
