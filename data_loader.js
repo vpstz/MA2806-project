bdb1dca5d0233c7dd9b43034279624d4cc423217
vpstz <vpstz214@gmail.com>
2026-05-02 03:01:46 +0100

    Added usage by country

--- data_loader.js
+++ data_loader.js
@@ -162,6 +162,7 @@ async function load_datasets() {
     const datasets = {
         usage: {
             ethnicity: await readData("datasets/ethnicity-usage.csv"),
+            country: await readData("datasets/usage-by-country.csv"),
             region: await readData("datasets/regional-usage.csv"),
             sex: await readData("datasets/usage-by-sex.csv"),
             age: await readData("datasets/by-age.csv"),
